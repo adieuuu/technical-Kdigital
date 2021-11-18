de3ca67d4654694af00764889e986a8cff073cd5
Alif Dhiashidqi Prawira <56311278+adieuuu@users.noreply.github.com>
2021-11-18 22:18:30 +0700

    coba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 function App() {
+  const [topStoriesData, setTopStoriesData] = useState([])
+  const [arrData, setArrData] = useState([{}])
+  const [tanda, setTanda] = useState(false)
+
+  useEffect(() => {
+    axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+    .then((res) => {
+      setTopStoriesData(res.data)
+    })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{height: 47, backgroundColor: 'skyblue', alignContent: 'center'}}>
+        <div style={{fontSize: 32, fontWeight: 'bold'}}>Top Stories</div>
+      </div>
+
+      <ImageList sx={{ width: '100%', height: 450 }} cols={2} rowHeight={164}>
+      {topStoriesData.map((item, index) => (
+        <ImageListItem key={index} style={{backgroundColor: 'red'}}>
+          <h3>{item}</h3>
+        </ImageListItem>
+      ))}
+      </ImageList>
+
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const headersStories = () => {
+  return (
+    <div style={{width: '100%', backgroundColor: 'red'}}>
+      <h1 style={{textAlign: 'center'}}>Top Stories</h1>
+    </div>
+  )
+}
+
+export default headersStories
